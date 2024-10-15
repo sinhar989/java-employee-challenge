c3ff620b1fcec874c74b51cf3c2c51e0d5c43e64
sinhar989 <49998566+sinhar989@users.noreply.github.com>
2024-10-15 10:18:14 +0530

    first commit

---
+++ src/main/java/com/example/rqchallenge/employees/IEmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.rqchallenge.employees;
+
+import com.example.rqchallenge.exception.EmployeeNotFoundException;
+import com.example.rqchallenge.model.Employee;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public interface IEmployeeController {
+
+    @GetMapping()
+    ResponseEntity<List<Employee>> getAllEmployees() throws IOException;
+
+    @GetMapping("/search/{searchString}")
+    ResponseEntity<List<Employee>> getEmployeesByNameSearch(@PathVariable String searchString);
+
+    @GetMapping("/{id}")
+    ResponseEntity<Employee> getEmployeeById(@PathVariable String id) throws EmployeeNotFoundException;
+
+    @GetMapping("/highestSalary")
+    ResponseEntity<Integer> getHighestSalaryOfEmployees();
+
+    @GetMapping("/topTenHighestEarningEmployeeNames")
+    ResponseEntity<List<String>> getTopTenHighestEarningEmployeeNames();
+
+    @PostMapping()
+    ResponseEntity<String> createEmployee(@RequestBody Map<String, Object> employeeInput);
+
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> deleteEmployeeById(@PathVariable String id);
+
+}
